73c7f8cd44bd4843f0bb855f1172b51abbaf4e25
Victor <victor1@ualberta.ca>
2019-11-08 12:31:32 -0700

    Added tests for adding and editing moods.

---
+++ MoodSpace/app/src/androidTest/java/com/example/moodspace/AddMoodTests.java
@@ -0,0 +1,100 @@
+package com.example.moodspace;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsAnything.anything;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class AddMoodTests {
+
+    private String username;
+    private String password;
+    private String angry;
+    private String happy;
+    private String sad;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule =
+            new ActivityTestRule<>(LoginActivity.class);
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        username = "e";
+        password = "e";
+        angry = new String(Character.toChars(0x1F621));
+        happy = new String(Character.toChars(0x1F604));
+        sad = new String(Character.toChars(0x1F62D));
+    }
+
+    @Test
+    public void addMoods() throws InterruptedException {
+        onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
+
+        onView(withId(R.id.login_btn)).perform(click());
+        Thread.sleep(1500);
+        // Add Happy Mood with Alone social situation and set reason to alone time.
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.emotionSelector)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.situationSelector)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.reason_text)).perform(typeText("alone time"), closeSoftKeyboard());
+        onView(withId(R.id.saveBtn)).perform(click());
+
+        // Add Angry Mood with Another person situation and set reason to fighting.
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.emotionSelector)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.situationSelector)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.reason_text)).perform(typeText("fighting"), closeSoftKeyboard());
+        onView(withId(R.id.saveBtn)).perform(click());
+
+        // Add Sad Mood with two or more people social situation and set reason to movie night.
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.emotionSelector)).perform(click());
+        onData(anything()).atPosition(3).perform(click());
+        onView(withId(R.id.situationSelector)).perform(click());
+        onData(anything()).atPosition(3).perform(click());
+        onView(withId(R.id.reason_text)).perform(typeText("Movie night"), closeSoftKeyboard());
+        onView(withId(R.id.saveBtn)).perform(click());
+
+        // Do not select a mood
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.saveBtn)).perform(click());
+        onView(withText("Select an Emotion")).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.backBtn)).perform(click());
+
+        //Check that the first mood in the list view is the sad mood, has two to several people as social situation and reason is movie night.
+        onData(anything())
+                .inAdapterView(allOf(withId(R.id.moodList), isCompletelyDisplayed()))
+                .atPosition(0).perform(click());
+        onView(withId(R.id.emotionSelector)).check(matches(not(withSpinnerText(containsString(angry)))));
+        onView(withId(R.id.emotionSelector)).check(matches(not(withSpinnerText(containsString(happy)))));
+        onView(withId(R.id.situationSelector)).check(matches(withSpinnerText(containsString("With two to several people"))));
+        onView(withId(R.id.reason_text)).check(matches(withText(containsString("Movie night"))));
+    }
+}
