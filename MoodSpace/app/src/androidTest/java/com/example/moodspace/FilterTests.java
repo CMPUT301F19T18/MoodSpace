f33c63a81f5f7a19203e81cd1621c5f25dc098e1
Liam Suits <suitsliam@gmail.com>
2019-11-07 23:35:58 -0700

    Sign Up and Delete
    Checks if the correct Moods has been filtered

--- MoodSpace/app/src/androidTest/java/com/example/moodspace/FilterTests.java
+++ MoodSpace/app/src/androidTest/java/com/example/moodspace/FilterTests.java
@@ -3,11 +3,17 @@ package com.example.moodspace;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.Checkable;
 
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
@@ -34,6 +40,7 @@ import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsNot.not;
 
@@ -43,6 +50,8 @@ public class FilterTests {
     private String username;
     private String password;
     private String angry;
+    private String happy;
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Rule
     public ActivityTestRule<LoginActivity> activityRule =
@@ -53,10 +62,12 @@ public class FilterTests {
         username = "Person";
         password = "password";
         angry = new String(Character.toChars(0x1F621));
+        happy = new String(Character.toChars(0x1F604));
     }
 
     @Test
     public void Filter() throws InterruptedException {
+        onView(withId(R.id.signup_link)).perform(click());
         onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
         onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
 
@@ -82,15 +93,20 @@ public class FilterTests {
                 .atPosition(1).perform(click());
 
         onView(Matchers.allOf(withId(android.R.id.button1), withText("OK"), childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-
-        Thread.sleep(3000);
+                childAtPosition(
+                        withClassName(is("android.widget.ScrollView")),
+                        0),
+                3))).perform(scrollTo(), click());
 
         onView(withId(R.id.moodList))
                 .check(matches(not(hasDescendant(withText(containsString(angry))))));
+
+        onView(withId(R.id.moodList))
+                .check(matches(hasDescendant(withText(containsString(happy)))));
+
+        db.collection("users")
+                .document(username)
+                .delete();
     }
 
     private static Matcher<View> childAtPosition(
