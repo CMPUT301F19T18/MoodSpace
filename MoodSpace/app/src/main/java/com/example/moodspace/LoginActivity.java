1a8b6cfef4532c8872665e120cba5fc49c6a43ab
Victor <victor1@ualberta.ca>
2019-11-14 18:47:53 -0700

    New login screen and tentative signup page.

--- MoodSpace/app/src/main/java/com/example/moodspace/LoginActivity.java
+++ MoodSpace/app/src/main/java/com/example/moodspace/LoginActivity.java
@@ -5,11 +5,10 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -23,6 +22,8 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         //ActionBar actionBar = getSupportActionBar();
         //actionBar.hide();
@@ -32,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         final AppCompatEditText username = findViewById(R.id.username);
         final AppCompatEditText password = findViewById(R.id.password);
         final AppCompatEditText veri_password = findViewById(R.id.password_veri);
+        final Button logOut = findViewById(R.id.nav_item_log_out);
 
         uc = new UserController(LoginActivity.this);
 
@@ -55,68 +57,34 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginAttempt();
-            }
-        });
+                String passwordText = password.getText().toString().trim();
+                String usernameText = username.getText().toString().trim();
 
-        password.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keycode, KeyEvent event) {
-                if (keycode == KeyEvent.KEYCODE_ENTER) {
-                    closekeyboard();
-                    loginButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            loginAttempt();
-                        }
-                    });
-                }
-                return false;
-            }
-        });
-    }
-
-    private void loginAttempt() {
-        final AppCompatEditText username = findViewById(R.id.username);
-        final AppCompatEditText password = findViewById(R.id.password);
-
-        String passwordText = password.getText().toString().trim();
-        String usernameText = username.getText().toString().trim();
-        closekeyboard();
-
-        if (LoginActivity.this.inLoginState) {
-            if (usernameText.length() > 0 && passwordText.length() > 0) {
-                uc.loginUser(new User(usernameText, passwordText));
-            } else {
-                Toast.makeText(LoginActivity.this,
-                        "Please enter a username and a password", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            final AppCompatEditText veri_password = findViewById(R.id.password_veri);
-            String veriPasswordText = veri_password.getText().toString().trim();
-            if (usernameText.length() > 0 && passwordText.length() > 0 && veriPasswordText.length() > 0) {
-                if (passwordText.equals(veriPasswordText)) {
-                    uc.checkUserExists(new User(usernameText, passwordText));
+                if (LoginActivity.this.inLoginState) {
+                    if (usernameText.length() > 0 && passwordText.length() > 0) {
+                        uc.loginUser(new User(usernameText, passwordText));
+                    } else {
+                        Toast.makeText(LoginActivity.this,
+                                "Please enter a username and a password", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(LoginActivity.this,
-                            "Please enter a matching password", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(LoginActivity.this,
-                        "Please enter a username, a password, and a password verification.",
-                        Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+                    String veriPasswordText = veri_password.getText().toString().trim();
+                    if (usernameText.length() > 0 && passwordText.length() > 0 && veriPasswordText.length() > 0) {
+                        if (passwordText.equals(veriPasswordText)) {
+                            uc.checkUserExists(new User(usernameText, passwordText));
+                        } else {
+                            Toast.makeText(LoginActivity.this,
+                                    "Please enter a matching password", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this,
+                                "Please enter a username, a password, and a password verification.",
+                                Toast.LENGTH_SHORT).show();
+                    }
 
-    private void closekeyboard() {
-        View view = this.getCurrentFocus();
-        if (view!= null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
+                }
+            }
+        });
 
     }
 }
-
-
