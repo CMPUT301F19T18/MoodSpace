e160231dbebc20eb6951c2c135ae5c2957c547bb
Victor <victor1@ualberta.ca>
2019-11-29 08:15:10 -0700

    Fixed up add test and added more thread sleeps for stability.

--- MoodSpace/app/src/androidTest/java/com/example/moodspace/AddMoodTest.java
+++ MoodSpace/app/src/androidTest/java/com/example/moodspace/AddMoodTest.java
@@ -21,6 +21,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsAnything.anything;
 import static org.hamcrest.core.IsNot.not;
@@ -62,6 +63,11 @@ public class AddMoodTest {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule =
             new ActivityTestRule<>(LoginActivity.class);
+
+    @Rule
+    public ActivityTestRule<AddEditActivity> mActivityRule2 =
+            new ActivityTestRule<>(AddEditActivity.class);
+
     @Before
     public void initValidString() {
         // Specify a valid string.
@@ -78,7 +84,7 @@ public class AddMoodTest {
         onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
 
         onView(withId(R.id.login_btn)).perform(click());
-        Thread.sleep(1500);
+        Thread.sleep(3000);
 //         Add Enjoyment Mood with Alone social situation and set reason to alone time.
         onView(withId(R.id.addMoodButton)).perform(click());
         onView(withId(R.id.emotionSelector)).perform(click());
@@ -88,7 +94,7 @@ public class AddMoodTest {
         onView(withId(R.id.reason_text)).perform(typeText("alone time"), closeSoftKeyboard());
         onView(withId(R.id.saveBtn)).perform(click());
 
-        Thread.sleep(1500);
+        Thread.sleep(250);
 
         // Add Anger Mood with Another person situation and set reason to fighting.
         onView(withId(R.id.addMoodButton)).perform(click());
@@ -99,7 +105,7 @@ public class AddMoodTest {
         onView(withId(R.id.reason_text)).perform(typeText("fighting"), closeSoftKeyboard());
         onView(withId(R.id.saveBtn)).perform(click());
 
-        Thread.sleep(1500);
+        Thread.sleep(250);
 
         // Add Sad Mood with two or more people social situation and set reason to movie night.
         onView(withId(R.id.addMoodButton)).perform(click());
@@ -110,24 +116,24 @@ public class AddMoodTest {
         onView(withId(R.id.reason_text)).perform(typeText("Movie night"), closeSoftKeyboard());
         onView(withId(R.id.saveBtn)).perform(click());
 
-        Thread.sleep(1500);
+        Thread.sleep(250);
 
         // Do not select a mood
         onView(withId(R.id.addMoodButton)).perform(click());
         onView(withId(R.id.saveBtn)).perform(click());
-        onView(withText("Select an emotion")).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+        onView(withText("Select an emotion")).inRoot(withDecorView(not(mActivityRule2.getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
         onView(withId(R.id.backBtn)).perform(click());
 
-        Thread.sleep(1500);
+        Thread.sleep(500);
 
-//        Check that the first mood in the list view is the sad mood, has two to several people as social situation and reason is movie night.
+        // Check that the first mood in the list view is the sad mood, has two to several people as social situation and reason is movie night.
         onData(anything())
                 .inAdapterView(allOf(withId(R.id.moodList), isCompletelyDisplayed()))
                 .atPosition(0).perform(click());
-
         onView(withId(R.id.emotionSelector)).check(matches(withChild(withText(containsString(sadness)))));
         onView(withId(R.id.situationText)).check(matches(withChild(withText(containsString("With two to several people")))));
         onView(withId(R.id.reason_text)).check(matches(withText(containsString("Movie night"))));
+        Thread.sleep(1500);
     }
 }
