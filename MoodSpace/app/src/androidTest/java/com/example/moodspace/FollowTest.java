4971a6859dd6c81578f896ba0124de4efdbf5960
Liam Suits <suitsliam@gmail.com>
2019-11-28 17:34:22 -0700

    Test Logs Out

---
+++ MoodSpace/app/src/androidTest/java/com/example/moodspace/FollowTest.java
@@ -0,0 +1,130 @@
+package com.example.moodspace;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FollowTest {
+    private String username = "TEST_FollowUITest";
+    private String password = "p";
+
+    private String username1 = "TEST_FollowUI2";
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityRule =
+            new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void follow() throws InterruptedException {
+        onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
+
+        onView(withId(R.id.login_btn)).perform(click());
+        Thread.sleep(1000);
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.toolbar),
+                                childAtPosition(
+                                        withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0)),
+                        3),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction tabView2 = onView(
+                allOf(withContentDescription("Send Request"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.tab_layout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        tabView2.perform(click());
+
+        onView(withId(R.id.username)).perform(typeText("TEST_FollowUI2"), closeSoftKeyboard());
+        onView(withId(R.id.request_btn)).perform(click());
+
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0)),
+                        5),
+                        isDisplayed()));
+        navigationMenuItemView2.perform(click());
+
+        ViewInteraction navigationMenuItemView1 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0)),
+                        6),
+                        isDisplayed()));
+        navigationMenuItemView1.perform(click());
+
+        onView(withId(R.id.username)).perform(typeText(username1), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
+
+        Thread.sleep(1000);
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
