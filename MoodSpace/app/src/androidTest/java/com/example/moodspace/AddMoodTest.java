54c87b33add045530482cfbc087ed157aa548c62
Victor <victor1@ualberta.ca>
2019-11-28 22:44:38 -0700

    Fixed Add Moods.

--- MoodSpace/app/src/androidTest/java/com/example/moodspace/AddMoodTest.java
+++ MoodSpace/app/src/androidTest/java/com/example/moodspace/AddMoodTest.java
@@ -1,5 +1,8 @@
 package com.example.moodspace;
 
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.test.rule.ActivityTestRule;
 
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -13,9 +16,15 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.IsAnything.anything;
@@ -25,25 +34,22 @@ public class AddMoodTest {
 
     private String username;
     private String password;
-    private String angry;
+    private String anger;
     private String enjoyment;
-    private String sad;
+    private String sadness;
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Rule
     public ActivityTestRule<LoginActivity> mActivityRule =
             new ActivityTestRule<>(LoginActivity.class);
-    @Rule
-    public ActivityTestRule<AddEditActivity> mActivityRule2 =
-            new ActivityTestRule<>(AddEditActivity.class);
     @Before
     public void initValidString() {
         // Specify a valid string.
         username = "AddTest";
         password = "AddTest";
-        angry = new String(Character.toChars(0x1F621));
+        anger = new String(Character.toChars(0x1F621));
         enjoyment = new String(Character.toChars(0x1F604));
-        sad = new String(Character.toChars(0x1F62D));
+        sadness = new String(Character.toChars(0x1F62D));
     }
 
     @Test
@@ -53,57 +59,56 @@ public class AddMoodTest {
 
         onView(withId(R.id.login_btn)).perform(click());
         Thread.sleep(1500);
-        // Add Enjoyment Mood with Alone social situation and set reason to alone time.
+//         Add Enjoyment Mood with Alone social situation and set reason to alone time.
         onView(withId(R.id.addMoodButton)).perform(click());
         onView(withId(R.id.emotionSelector)).perform(click());
-        onData(anything()).atPosition(0).perform(click());
-//        onData(instanceOf(Emotion.class)).atPosition(1).perform(click());
+        onData(anything()).atPosition(1).perform(click());
         onView(withId(R.id.situationSelector)).perform(click());
         onData(anything()).atPosition(0).perform(click());
         onView(withId(R.id.reason_text)).perform(typeText("alone time"), closeSoftKeyboard());
         onView(withId(R.id.saveBtn)).perform(click());
-//
-//        Thread.sleep(1500);
 
-//        // Add Angry Mood with Another person situation and set reason to fighting.
-//        onView(withId(R.id.addMoodButton)).perform(click());
-//        onView(withId(R.id.emotionSelector)).perform(click());
-//        onData(anything()).atPosition(1).perform(click());
-//        onView(withId(R.id.situationSelector)).perform(click());
-//        onData(anything()).atPosition(1).perform(click());
-//        onView(withId(R.id.reason_text)).perform(typeText("fighting"), closeSoftKeyboard());
-//        onView(withId(R.id.saveBtn)).perform(click());
-//
-//        Thread.sleep(1500);
-//
-//        // Add Sad Mood with two or more people social situation and set reason to movie night.
-//        onView(withId(R.id.addMoodButton)).perform(click());
-//        onView(withId(R.id.emotionSelector)).perform(click());
-//        onData(anything()).atPosition(2).perform(click());
-//        onView(withId(R.id.situationSelector)).perform(click());
-//        onData(anything()).atPosition(2).perform(click());
-//        onView(withId(R.id.reason_text)).perform(typeText("Movie night"), closeSoftKeyboard());
-//        onView(withId(R.id.saveBtn)).perform(click());
-//
-//        Thread.sleep(1500);
-//
-//        // Do not select a mood
-//        onView(withId(R.id.addMoodButton)).perform(click());
-//        onView(withId(R.id.saveBtn)).perform(click());
-//        onView(withText("Select an emotion")).inRoot(withDecorView(not(mActivityRule2.getActivity().getWindow().getDecorView())))
-//                .check(matches(isDisplayed()));
-//        onView(withId(R.id.backBtn)).perform(click());
-//
-//        Thread.sleep(1500);
-//
-////        Check that the first mood in the list view is the sad mood, has two to several people as social situation and reason is movie night.
-//        onData(anything())
-//                .inAdapterView(allOf(withId(R.id.moodList), isCompletelyDisplayed()))
-//                .atPosition(0).perform(click());
-//        onView(withId(R.id.emotionSelector)).check(matches(not(withSpinnerText(containsString(angry)))));
-//        onView(withId(R.id.emotionSelector)).check(matches(not(withSpinnerText(containsString(enjoyment)))));
-//        onView(withId(R.id.situationSelector)).check(matches(withSpinnerText(containsString("With two to several people"))));
-//        onView(withId(R.id.reason_text)).check(matches(withText(containsString("Movie night"))));
+        Thread.sleep(1500);
+
+        // Add Anger Mood with Another person situation and set reason to fighting.
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.emotionSelector)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.situationSelector)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.reason_text)).perform(typeText("fighting"), closeSoftKeyboard());
+        onView(withId(R.id.saveBtn)).perform(click());
+
+        Thread.sleep(1500);
+
+        // Add Sad Mood with two or more people social situation and set reason to movie night.
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.emotionSelector)).perform(click());
+        onData(anything()).atPosition(3).perform(click());
+        onView(withId(R.id.situationSelector)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.reason_text)).perform(typeText("Movie night"), closeSoftKeyboard());
+        onView(withId(R.id.saveBtn)).perform(click());
+
+        Thread.sleep(1500);
+
+        // Do not select a mood
+        onView(withId(R.id.addMoodButton)).perform(click());
+        onView(withId(R.id.saveBtn)).perform(click());
+        onView(withText("Select an emotion")).inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.backBtn)).perform(click());
+
+        Thread.sleep(1500);
+
+//        Check that the first mood in the list view is the sad mood, has two to several people as social situation and reason is movie night.
+        onData(anything())
+                .inAdapterView(allOf(withId(R.id.moodList), isCompletelyDisplayed()))
+                .atPosition(0).perform(click());
+
+        onView(withId(R.id.emotionSelector)).check(matches(withChild(withText(containsString(sadness)))));
+        onView(withId(R.id.situationSelector)).check(matches(withChild(withText(containsString("With another person")))));
+        onView(withId(R.id.reason_text)).check(matches(withText(containsString("Movie night"))));
 
     }
 }
